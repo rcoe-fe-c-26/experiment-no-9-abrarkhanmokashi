51cf41af04bb339f54cd111d5d4635c4e4b63175
abrarkhanmokashi <abrarkhanmokashi@eng.rizvi.edu.in>
2026-02-20 14:16:29 +0530

    Implement factorial function in main.py
    Added a factorial function to compute the factorial of a given integer, including input handling and validation.

--- main.py
+++ main.py
@@ -7,3 +7,21 @@ print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+def factorial(n):
+    if n < 0:
+        return False
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+    
+number = int(input("Enter number: "))
+fact = factorial(number)
+if fact is False:
+    print(f"Factorial of {number} is not defined.")
+    
+else:
+    print(f"Factorial of {number} is {fact}.")
